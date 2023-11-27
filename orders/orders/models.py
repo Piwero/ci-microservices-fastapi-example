3b28c83d2315cfce4e182e551378393571b09068
piwero <piwero@gmail.com>
2023-11-27 11:55:16 +0000

    Create inital order item class

--- orders/orders/models.py
+++ orders/orders/models.py
@@ -2,6 +2,12 @@ from uuid import UUID, uuid4
 
 from pydantic import BaseModel
 
+from inventory.inventory.models import Product
+
+
+class OrderItem(BaseModel):
+    product: Product
+
 
 class Order(BaseModel):
     id: UUID = uuid4()
--- orders/tests/unit-tests/test_order_models.py
+++ orders/tests/unit-tests/test_order_models.py
@@ -1,4 +1,20 @@
-from orders.models import Order
+import pytest
+
+from inventory.inventory.models import Product
+from orders.models import Order, OrderItem
+
+
+@pytest.fixture
+def product_1():
+    product: Product = Product(name="Product 1", price=30.99)
+    return product
+
+
+class TestOrderItem:
+    def test_order_item_has_product_field(self, product_1):
+        order_item = OrderItem(product=product_1)
+
+        assert order_item.product == product_1
 
 
 class TestOrderModel:
@@ -6,4 +22,3 @@ class TestOrderModel:
         order: Order = Order()
 
         assert order.id
-
